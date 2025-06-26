5e6d310e6e48141ce97a39176761caf04a12a760
Fabien Taupin <taupinfabien2607@gmail.com>
2025-06-26 18:40:44 +0200

    feat: create SelectFilter component

---
+++ src/components/ArrayOfLogs/filtersInputs/SelectFilter.tsx
@@ -0,0 +1,43 @@
+interface Option<T> {
+  value: T;
+  label: string;
+}
+
+interface SelectFilterProps<T> {
+  id: string;
+  label: string;
+  options: Option<T>[];
+  setSelectedValue: (value: T) => void;
+}
+
+/**
+ * SelectFilter is a reusable dropdown component using a generic type T.
+ * This allows it to work with different kinds of options (like log levels or services)
+ * while keeping strong type safety and autocompletion.
+ */
+
+function SelectFilter<T extends string>({
+  id,
+  label,
+  options,
+  setSelectedValue,
+}: SelectFilterProps<T>) {
+  return (
+    <div id={`SelectFilter__${label}__container__${id}`} className="p-2 flex-1">
+      <label id={`SelectFilter__${label}__title__${id}`}>{label}</label>
+      <select
+        id={`SelectFilter__${label}__container__${id}`}
+        className="rounded border w-full p-1.5"
+        onChange={(e) => setSelectedValue(e.target.value as T)}
+      >
+        {options.map(({ value, label }) => (
+          <option key={value} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default SelectFilter;
