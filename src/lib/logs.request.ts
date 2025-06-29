0e6076dee90877cb096c7ee81cf57afbb4c4b490
Fabien Taupin <taupinfabien2607@gmail.com>
2025-06-29 23:28:59 +0200

    fix: add message parameters to get request in logs.requeset.ts

--- src/lib/logs.request.ts
+++ src/lib/logs.request.ts
@@ -18,14 +18,21 @@ interface LogsParams {
   date: string;
   level: LogLevelOption;
   service: LogServiceOption;
+  message: string;
 }
 
-export const getLogs = async ({ date, level, service }: LogsParams) => {
+export const getLogs = async ({
+  date,
+  level,
+  service,
+  message,
+}: LogsParams) => {
   const params = new URLSearchParams();
 
   if (date) params.set("date", date);
   if (level) params.set("level", level);
   if (service) params.set("service", service);
+  if (message) params.set("q", message);
 
   try {
     const response = await axios.get(
