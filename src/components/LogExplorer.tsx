af4451006b4156ac550a41098cd4e334588060c6
Fabien Taupin <taupinfabien2607@gmail.com>
2025-06-30 14:12:11 +0200

    fix: clean code in LogExplorer.tsx

--- src/components/LogExplorer.tsx
+++ src/components/LogExplorer.tsx
@@ -1,19 +1,21 @@
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { format } from "date-fns";
+
 import SearchBar from "./inputs/Searchbar";
-import { getLogs } from "../lib/logs.request";
 import ShowFiltersButton from "./inputs/ShowFiltersButton";
 import Timestamp from "./inputs/filtersInputs/Timestamp";
 import ManualLogEntryForm from "./ManualLogEntryForm";
 import SelectFilter from "./inputs/filtersInputs/SelectFilter";
+import MessageRow from "./MessageRow";
+
+import { getLogs } from "../lib/logs.request";
 import { levels, services } from "./inputs/filtersInputs/SelectFilter.utils";
 import type {
   LogLevelOption,
   LogServiceOption,
   LogsProps,
 } from "../lib/logs.utils";
-import { useQuery } from "@tanstack/react-query";
-import MessageRow from "./MessageRow";
-import { format } from "date-fns";
 
 interface LogExplorerProps {
   id: string;
@@ -26,6 +28,10 @@ interface PaginatedLogs {
   results: LogsProps[];
 }
 
+function formatedIsoDate(date: string): string {
+  return format(new Date(date), "yyyy/MM/dd");
+}
+
 function LogExplorer({ id }: LogExplorerProps) {
   const [showAdvancedFilters, setShowAdvancedFilter] = useState(false);
   const [year, setYear] = useState(["", "", "", ""]);
@@ -33,17 +39,14 @@ function LogExplorer({ id }: LogExplorerProps) {
   const [day, setDay] = useState(["", ""]);
   const [level, setLevel] = useState<LogLevelOption>("");
   const [service, setService] = useState<LogServiceOption>("");
-
-  function formatedIsoDate(date: string): string {
-    return format(new Date(date), "yyyy/MM/dd");
-  }
-
-  let date = "";
+  const [message, setMessage] = useState<string>("");
+  console.log(message);
 
   const fullYear = year.join("");
   const fullMonth = month.join("");
   const fullDay = day.join("");
 
+  let date = "";
   if (fullYear.length === 4) {
     date = fullYear;
     if (fullMonth.length === 2) {
@@ -54,17 +57,64 @@ function LogExplorer({ id }: LogExplorerProps) {
     }
   }
 
-  const filters = { date, level, service };
+  const filters = { date, level, service, message };
 
   const {
     data: logsData,
     isLoading,
     isError,
-    refetch,
   } = useQuery<PaginatedLogs, Error>({
     queryKey: ["logs", filters],
     queryFn: () => getLogs(filters),
   });
+
+  const RenderLogRows = () => {
+    if (isLoading) {
+      return (
+        <MessageRow
+          id={`LogExplorer__messageRow__isLoading__${id}`}
+          txt="Loading logs..."
+        />
+      );
+    }
+
+    if (isError) {
+      return (
+        <MessageRow
+          id={`LogExplorer__messageRow__isError__${id}`}
+          txt="Failed during fetching logs..."
+        />
+      );
+    }
+
+    if (!logsData?.results.length) {
+      return (
+        <MessageRow
+          id={`LogExplorer__messageRow__length__${id}`}
+          txt="No logs found."
+        />
+      );
+    }
+
+    return logsData?.results.map((log: LogsProps, index: number) => (
+      <tr key={`${index}__${log.id}`}>
+        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+          {formatedIsoDate(log.timestamp)}
+        </td>
+        <td className="px-6 py-4 whitespace-nowrap">
+          <span
+            className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full`}
+          >
+            {log.level}
+          </span>
+        </td>
+        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+          {log.service}
+        </td>
+        <td className="px-6 py-4 text-sm text-gray-500">{log.message}</td>
+      </tr>
+    ));
+  };
 
   return (
     <section
@@ -74,7 +124,11 @@ function LogExplorer({ id }: LogExplorerProps) {
       <ManualLogEntryForm id={`LogExplorer__manualLogEntryForm__${id}`} />
       <div className="flex flex-col gap-4">
         <h2 className="text-2xl text-gray-800 font-light">Logs System</h2>
-        <SearchBar id={`LogExplorer__searchBar__${id}`} />
+        <SearchBar
+          id={`LogExplorer__searchBar__${id}`}
+          setMessage={setMessage}
+          debounceDelay={400}
+        />
         <ShowFiltersButton
           id={`LogExplorer__showFiltersButton__${id}`}
           showAdvancedFilters={showAdvancedFilters}
@@ -108,7 +162,6 @@ function LogExplorer({ id }: LogExplorerProps) {
             options={services}
           />
         </div>
-        <button onClick={() => refetch()}>search</button>
       </div>
 
       <div className="overflow-x-auto bg-white rounded-lg flex flex-col gap-2 max-h-64 overflow-y-auto">
@@ -118,58 +171,18 @@ function LogExplorer({ id }: LogExplorerProps) {
         <table className="min-w-full bg-gray-300">
           <thead className="bg-gray-300">
             <tr>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Timestamp
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Level
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Service
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Message
-              </th>
+              {[].map((header) => (
+                <th
+                  key={header}
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  {header}
+                </th>
+              ))}
             </tr>
           </thead>
           <tbody className="bg-white divide-y divide-gray-200">
-            {isLoading ? (
-              <MessageRow
-                id={`LogExplorer__messageRow__isLoading__${id}`}
-                txt="Loading logs..."
-              />
-            ) : isError ? (
-              <MessageRow
-                id={`LogExplorer__messageRow__isError__${id}`}
-                txt="Failed during fetching logs..."
-              />
-            ) : logsData?.results.length === 0 ? (
-              <MessageRow
-                id={`LogExplorer__messageRow__length__${id}`}
-                txt="No logs found."
-              />
-            ) : (
-              logsData?.results.map((log: LogsProps, index: number) => (
-                <tr key={`${index}__${log.id}`}>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                    {formatedIsoDate(log.timestamp)}
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    <span
-                      className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full`}
-                    >
-                      {log.level}
-                    </span>
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                    {log.service}
-                  </td>
-                  <td className="px-6 py-4 text-sm text-gray-500">
-                    {log.message}
-                  </td>
-                </tr>
-              ))
-            )}
+            {RenderLogRows()}
           </tbody>
         </table>
       </div>
